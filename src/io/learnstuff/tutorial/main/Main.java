f2ad21b4eb99a1b1a1cc14c2cb92711b7761fd8b
idamian23 <damian.ionut23@yahoo.ro>
2021-03-10 09:55:06 +0200

    Assignment1 - Problem 1

--- src/io/learnstuff/tutorial/main/Main.java
+++ src/io/learnstuff/tutorial/main/Main.java
@@ -3,9 +3,15 @@ package io.learnstuff.tutorial.main;
 public class Main {
     public static void main(String[] args) {
 
+
+        //  Assignment 1 - Exercise 1
         Counter counter = new Counter();
+
+        //Array of Integers
         int[] arr = {1, 2, 3, 4, 2, 2, 2, 2};
         System.out.println(counter.count(arr, 2));
+
+        //Array of Strings
         String[] arrString = {"abc", "asfasd", null};
         System.out.println(counter.count(arrString, "abc"));
     }
