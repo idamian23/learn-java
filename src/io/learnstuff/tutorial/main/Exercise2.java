961929278774b128ef49dec2c5bd3dadd8376c98
idamian23 <damian.ionut23@yahoo.ro>
2021-03-10 13:06:10 +0200

    Assignment1 - Solutiile pentru primele 3 exercitii

---
+++ src/io/learnstuff/tutorial/main/Exercise2.java
@@ -0,0 +1,58 @@
+package io.learnstuff.tutorial.main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args){
+
+        // Assignment 1 - Exercise 2
+        // Read an array and a matrix from the keyboard
+        // (requires the use of scanner)
+
+        int length;
+        int rows;
+        int columns;
+        Scanner scanner = new Scanner(System.in);
+
+        // For Array
+        System.out.println("Array length: ");
+        length = scanner.nextInt();
+        int[] array = new int[length];
+
+        System.out.println("Introduce your array elements here: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Your array's elements are: ");
+        System.out.print("[ ");
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.print("]\n");
+
+        // For Matrix
+        Random random = new Random();
+        System.out.println("Enter number of rows for the matrix: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter number of columns for the matrix: ");
+        columns = scanner.nextInt();
+
+
+        int[][] matrix = new int[rows][columns];
+        System.out.println();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.println("Enter the elements for the matrix: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Elements for the matrix ...");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++)
+                System.out.print(matrix[i][j] + "  ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/io/learnstuff/tutorial/main/Exercise3.java
@@ -0,0 +1,38 @@
+package io.learnstuff.tutorial.main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    //Assignment 1 - Exercise 3
+    // Use the Random class to generate elements for a matrix for which you read
+    //length and height from the keyboard
+
+    public static void main(String[] args){
+        Random random = new Random();
+        int rows;
+        int columns;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of rows for the matrix: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter number of columns for the matrix: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter the bounderies of the matrix: ");
+        int bound = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = random.nextInt(bound);
+            }
+        }
+
+        System.out.println("Generating elements for the matrix ...");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++)
+                System.out.print(matrix[i][j] + "  ");
+            System.out.println();
+        }
+    }
+}
