02c465fa4307c2c61227ecc01d88b4dfbbbd9308
idamian23 <damian.ionut23@yahoo.ro>
2021-03-10 17:24:06 +0200

    Assignment1 - Solutia pentru exercitiul 1

---
+++ src/io/learnstuff/tutorial/main/Exercise5.java
@@ -0,0 +1,43 @@
+package io.learnstuff.tutorial.main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int length = random.nextInt(100);
+        int toFind;
+
+
+        int[] arr = new int[length];
+
+        for(int i = 0; i < length; i++){
+            arr[i] = random.nextInt(100);
+        }
+        System.out.println("The automated generated array is :");
+        System.out.print("[ ");
+        for(int i = 0; i < length; i++){
+            System.out.print( arr[i] + " ");
+        }
+        System.out.print(" ]");
+
+        System.out.println("\nChoose you number to find: ");
+        toFind = scanner.nextInt();
+
+        int count = 0;
+        for(int i = 0; i < length; i++){
+            if(arr[i] == toFind){
+               count++;
+            }
+        }
+        if(count > 0){
+              System.out.println("Your number is in the array!");
+
+        }else{
+            System.out.println("Sorry, it is not in the array!");
+        }
+        }
+    }
+
