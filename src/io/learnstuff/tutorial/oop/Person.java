a3defc007f7f222420299b583500c33a080ccd7a
idamian23 <damian.ionut23@yahoo.ro>
2021-03-16 11:39:28 +0200

    refactoring Person Class

--- src/io/learnstuff/tutorial/oop/Person.java
+++ src/io/learnstuff/tutorial/oop/Person.java
@@ -4,6 +4,14 @@ public class Person {
 
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     // code to execute
     public void doStuff(){
             System.out.println("A real person does things!");
