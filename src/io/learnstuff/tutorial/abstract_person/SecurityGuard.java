401de79f56fed27a2f9debb408cfc39db777d7c6
idamian23 <damian.ionut23@yahoo.ro>
2021-03-26 17:34:34 +0200

    Security Guard

---
+++ src/io/learnstuff/tutorial/abstract_person/SecurityGuard.java
@@ -0,0 +1,36 @@
+package io.learnstuff.tutorial.abstract_person;
+
+public class SecurityGuard extends Person{
+    public SecurityGuard(String name) {
+        super(name);
+    }
+
+    @Override
+    public void introduceYourself() {
+        System.out.println("My name is " + name + ".");
+    }
+
+    @Override
+    public void nameYourJob() {
+        System.out.println("My job is to keep your data safe.");
+    }
+
+    public void securityGuardEncryptionMethod(String textToEncrypt, int key){
+        char[] chars = textToEncrypt.toCharArray();
+        for(char c : chars){
+            c += key; // char and int works togheter
+            System.out.print(c);
+        }
+        System.out.println("\n");
+    }
+    public void securityGuardDecryptionMethod(String textToDecrypt, int key){
+
+        char[] chars = textToDecrypt.toCharArray();
+        for(char c : chars){
+            c -= key;
+            System.out.print(c);
+
+        }
+
+    }
+}
