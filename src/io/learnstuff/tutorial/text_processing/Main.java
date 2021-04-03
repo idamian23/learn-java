fef8cfa7be5f9955666b7b641927c4448349190e
idamian23 <damian.ionut23@yahoo.ro>
2021-04-03 12:28:05 +0300

    TEXT PROCESSING METHODS DONE

--- src/io/learnstuff/tutorial/text_processing/Main.java
+++ src/io/learnstuff/tutorial/text_processing/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         var textProcessing = new TextProcessing();
         textProcessing.split("This is going to split the string and reverse the chosen word.", "reverse");
-        TextProcessing.println(textProcessing.reverse("xes", "I want lots of xes with dirty bitches."));
+        TextProcessing.println(textProcessing.reverse("Good", "Good."));
         TextProcessing.println(textProcessing.reverse("15","Am de ridicat un colet in 15 minute."));
     }
 }
