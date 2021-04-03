1aebc1ff15f53b27806f1ba0868f47a9a7aeffee
idamian23 <damian.ionut23@yahoo.ro>
2021-04-03 12:31:47 +0300

    TEXT PROCESSING METHODS DONE

--- src/io/learnstuff/tutorial/text_processing/Main.java
+++ src/io/learnstuff/tutorial/text_processing/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         var textProcessing = new TextProcessing();
         textProcessing.split("This is going to split the string and reverse the chosen word.", "reverse");
-        TextProcessing.println(textProcessing.reverse("Good", "Good."));
+        TextProcessing.println(textProcessing.reverse("atunci", "Cea mai mare glorie nu o dobândești atunci când nu ești doborât, ci atunci când te ridici după ce ai căzut"));
         TextProcessing.println(textProcessing.reverse("15","Am de ridicat un colet in 15 minute."));
     }
 }
