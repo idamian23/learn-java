c886210cf0e34f7ae9df6bf6d2210e26cef6dee2
idamian23 <damian.ionut23@yahoo.ro>
2021-04-05 21:58:07 +0300

    Palindrome

---
+++ src/Palindrome/Palindrome.java
@@ -0,0 +1,18 @@
+package Palindrome;
+
+public class Palindrome {
+    public static void main(String[] arg){
+        System.out.println(Palindrome.isPalindrome("add"));
+    }
+
+    public static boolean isPalindrome(String string){
+        int length = string.length();
+        String reverse = "";
+        for(int i = length - 1; i >= 0; i--)
+            reverse = reverse + string.charAt(i);
+        if(string.equals(reverse))
+            return true;
+        else
+           return false;
+    }
+}
